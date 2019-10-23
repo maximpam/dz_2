73876e853d273230da49c2c6a0958c45e27e1c28
maximpam <maximpam@ukr.net>
2019-10-23 18:41:58 +0300

    Initizalisation

---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$login="QWERTY";
+$email="habib@mail.ru";
+$languages=[
+    'EN'=>'English',
+    'RU'=>'Russian',
+    'UA'=>'Ukrainian'
+];
+
+foreach ($languages as $key => $value) {
+    $option .= "<option value = '$key' > $value</option >";
+    }
+
+$render = "<form>
+<span>Email</span>
+<input type='text' name='email' value='$email'><br>
+<span>Login</span>
+<input type='text' name='login' value='$login'><br>
+<span>Password</span>
+<input type='password' name='password'><br>
+<span>Choose your language</span>
+<select>
+    $option
+</select><br>
+<button type='submit'> Register</button>
+</form>";
+
+echo $render;
